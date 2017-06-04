d861baee69f0f1fba681bfe8075878066be89cc7
gabietaylor <gabie.taylor@yahoo.com>
2017-06-04 16:17:00 -0400

    final version

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,9 +1,12 @@
+$(document).ready(function() {
+
 //intial topics
 var topics = ["Space", "Milky Way", "Astronaut", "Galaxies", "Neil DeGrasse Tyson", "Bill Nye", "Nikola Tesla"];
 console.log(topics);
+var animate = 0;
 //displaying info
 
-  function displayTopicInfo() {
+  function displayTopic() {
 
     //grabbing info
     var space = $(this).attr("data-topic");
@@ -12,28 +15,51 @@ console.log(topics);
     console.log(space);
     console.log(queryURL);
 
-    //Ajax for animation
+    //Ajax grab
     $.ajax({
       url: queryURL,
       method: "GET"
     }).done(function(response) {
       var results = response.data;
       console.log(results);
-
+      //pulling the still imgs, ratings and putitng them in a div
       for (var i = 0; i < results.length; i++) {
         var imageDiv = $("<div class='item'>");
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating);
-        var spaceImage = $("<img>");
-        spaceImage.attr("src", results[i].images.fixed_height.url);
+        var spaceImage = $("<img>").val(i);
+        spaceImage.attr("src", results[i].images.fixed_height_still.url);
+        spaceImage.addClass("gifs");
+        //ap/prepending everything to my own div
         imageDiv.append(p);
         imageDiv.prepend(spaceImage);
         $("#images").prepend(imageDiv);
       }
-});
-  }
+      //animate gifs
+      //psuedo code
+      // pull the still img from api like the animation then apply an on click function to get the div to switch
+      //bt the animation and still img/ boolean(t/f)
+        $(".gifs").on("click", function(event) {
+          console.log("gifs click");
+          console.log(this.value);
+          if (animate === 0) {
+            var imgval = this.value;
+            console.log(imgval);
+            $(this).attr("src", results[imgval].images.fixed_height.url);
+            animate++
+            console.log(animate);
+            }else{
+            var imgval = this.value;
+            $(this).attr("src", results[imgval].images.fixed_height_still.url);
+            animate--
+            console.log(animate);
+          }
+        });
+})
+
+}
  
-//collect buttons
+//Collect Btns
 function renderButtons() {
  $(".collectBtns").empty();
  for (var i = 0; i < topics.length; i++) {
@@ -45,6 +71,7 @@ function renderButtons() {
  }
 }
 
+//Add Btn Submit
 $("#add-topic").on("click", function(event) {
   event.preventDefault();
   var topic = $("#topic-input").val().trim();
@@ -53,12 +80,6 @@ $("#add-topic").on("click", function(event) {
 });
   renderButtons();
 
-$(document).on("click", ".topic", displayTopicInfo);
+$(document).on("click", ".topic", displayTopic);
       renderButtons();
-
-//animate gifs
-//psuedo code
-// pull the still img from api like the animation then apply an on click function to get the div to switch
-//bt the animation and still img/ boolean(t/f)
-//Ajax for still
-
+});
